8267b6365ec47f46e07dcbabb4bcb439050e00fd
Jean Jourdain <jean.jourdain@mixio.com>
2018-09-24 18:54:46 +0200

    Use mixio modules in package

--- Package.swift
+++ Package.swift
@@ -8,22 +8,25 @@ let package = Package(
     ],
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
-        
+//      .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
+        .package(url: "https://github.com/mixio/core.git", .branch("mixio-dev")),
+
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
 
         // 🗄 Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", from: "1.2.0"),
+//      .package(url: "https://github.com/vapor/database-kit.git", from: "1.2.0"),
+        .package(url: "https://github.com/mixio/database-kit.git", .branch("mixio-dev")),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
-        
+
         // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
-        
+
         // *️⃣ Build SQL queries in Swift. Extensible, protocol-based design that supports DQL, DML, and DDL.
-        .package(url: "https://github.com/vapor/sql.git", from: "2.0.2"),
+//      .package(url: "https://github.com/vapor/sql.git", from: "2.0.0"),
+        .package(url: "https://github.com/mixio/sql.git", .branch("mixio-dev")),
     ],
     targets: [
         .target(name: "PostgreSQL", dependencies: ["Async", "Bits", "Core", "Crypto", "DatabaseKit", "NIO", "Service", "SQL"]),
